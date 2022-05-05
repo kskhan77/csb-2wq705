5bbdd77018fcfaa06ed4a9a0c875b761a362b325
Code Sandbox <hello@codesandbox.io>
2022-05-05 23:45:34 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.b2b42485cab6eb871ed5efd3bd77b4df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a9fadf1c935b0f1a2dc",
+    "url": "https://kskhan77.github.io/csb-2wq705/static/css/main.125e24e9.chunk.css"
+  },
+  {
+    "revision": "1a9fadf1c935b0f1a2dc",
+    "url": "https://kskhan77.github.io/csb-2wq705/static/js/main.1a9fadf1.chunk.js"
+  },
+  {
+    "revision": "8ab92db96799b580b505",
+    "url": "https://kskhan77.github.io/csb-2wq705/static/js/1.8ab92db9.chunk.js"
+  },
+  {
+    "revision": "16ddf0e6f344f4565eba",
+    "url": "https://kskhan77.github.io/csb-2wq705/static/js/runtime~main.16ddf0e6.js"
+  },
+  {
+    "revision": "452d42c26c64beb06825b45b4db9fa0c",
+    "url": "https://kskhan77.github.io/csb-2wq705/index.html"
+  }
+];
